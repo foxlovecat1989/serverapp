6bfff7ddf98293d1034ffa69cf9df45e5b310997
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-11-20 15:20:44 +0800

    refactor apiUrl

--- src/app/serveice/server.service.ts
+++ src/app/serveice/server.service.ts
@@ -12,7 +12,7 @@ import { Server } from '../interface/server';
 export class ServerService {
 
 
-  private readonly apiUrl = 'any';
+  private readonly apiUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
